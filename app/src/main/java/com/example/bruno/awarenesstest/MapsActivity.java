634c8f18777437dd3dc6081797f9ad0eb0d315f6
brunoba <brunobarros2014@gmail.com>
2017-06-17 17:14:15 -0300

    Location com Google Awareness

--- app/src/main/java/com/example/bruno/awarenesstest/MapsActivity.java
+++ app/src/main/java/com/example/bruno/awarenesstest/MapsActivity.java
@@ -3,6 +3,11 @@ package com.example.bruno.awarenesstest;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -12,6 +17,7 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.awareness.snapshot.LocationResult;
 import com.google.android.gms.awareness.snapshot.WeatherResult;
 import com.google.android.gms.awareness.state.Weather;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -25,13 +31,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,SensorEventListener {
 
     private GoogleMap mMap;
     private GoogleApiClient mClient;
 //    private Toast t;
 //    private Context context = getApplicationContext();
     private static String textHole = "Cuidado com o buraco!";
+    private static final int TIPO_SENSOR = Sensor.TYPE_ACCELEROMETER;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private static double SENSIBILIDADE_SENSOR_X = 1.2;
+    private static double SENSIBILIDADE_SENSOR_Y = 1.5;
+    private static double MIN_SENSOR_Z = 8;
+    private static double MAX_SENSOR_Z = 12;
+    private static String TAG = "Awareness";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(TIPO_SENSOR);
+
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        Log.i("teste",sensor.toString());
+
+
         mClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addApi(Awareness.API)
@@ -52,23 +74,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         LatLng casaDoCriador = new LatLng(-8.14049705, -34.90979254);
         LatLng unicap = new LatLng(-8.05557534,-34.88822222);
-
+//        LatLng lugar = new LatLng(-8.153492,-34.92);
 
         if (ContextCompat.checkSelfPermission(
                 this,
@@ -82,9 +94,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.setMyLocationEnabled(true);
-
         mMap.addMarker(new MarkerOptions().position(casaDoCriador).title(textHole));
         mMap.addMarker(new MarkerOptions().position(unicap).title(textHole));
+
+
+//        mMap.addMarker(new MarkerOptions().position(lugar).title("Aqui o lugar misterioso!"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(casaDoCriador));
 
     }
@@ -125,6 +139,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        float sensorX = -event.values[0];
+        float sensorY = event.values[1];
+        float sensorZ = event.values[2];
+
+        if(checkTheConditions(sensorX,sensorY,sensorZ)){
+
+            CharSequence text = "Buraco!";
+            Toast t = Toast.makeText( getApplicationContext(), text, Toast.LENGTH_SHORT);
+            t.show();
+
+            Log.i("teste","Buraco detectado!");
+
+            // UMA VEZ DETECTADO UM BURACO, ESTE DEVE SER SALVO
+            Awareness.SnapshotApi.getLocation(mClient)
+                    .setResultCallback(new ResultCallback<LocationResult>() {
+                        @Override
+                        public void onResult(@NonNull LocationResult locationResult) {
+
+                            if (!locationResult.getStatus().isSuccess()) {
+                                Log.e(TAG, "Could not get location.");
+                                return;
+                            }
+                            Location location = locationResult.getLocation();
+                            Log.i(TAG, "Lat: " + location.getLatitude() + ", Lon: " + location.getLongitude());
+                        }
+                    });
+        }
+    }
+
+    //METODO FEITO PARA IDENTIFICAR SE HOUVE A DETECÇAO DE UM BURACO
+    private boolean checkTheConditions(float x, float y, float z){
+
+        if(x > SENSIBILIDADE_SENSOR_X && y > SENSIBILIDADE_SENSOR_Y &&
+                (MAX_SENSOR_Z > z &&  z < MIN_SENSOR_Z) ||
+                (-MAX_SENSOR_Z > z &&  z < -MIN_SENSOR_Z)){
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
